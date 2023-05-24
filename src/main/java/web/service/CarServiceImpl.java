ce2813bf9965caac6e57a3854fac61fc3e81940d
Alexopr <74917007+Alexopr@users.noreply.github.com>
2023-05-24 22:52:28 +0300

    Add files via upload

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,19 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import web.dao.CarDaoImpl;
+import web.model.Car;
+
+import java.util.List;
+@Service
+public class CarServiceImpl implements CarService {
+
+    @Autowired
+    private CarDaoImpl carDao;
+    @Override
+    public List<Car> getCars(int count) {
+        return carDao.getCars(count);
+    }
+}
